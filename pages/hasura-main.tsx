092cc2850b12f535abda18f1376ed9acfafefe17
fractaaal <58377055+fractaaal@users.noreply.github.com>
2021-12-31 15:57:09 +0900

    fetch cache

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -4,3 +4,36 @@ import { useQuery } from '@apollo/client'
 import { GET_USERS } from '../queries/queries'
 import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: 'network-only',
+  })
+
+  if (error) {
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">hasura main page</p>
+      {console.log(data)}
+      {data?.users.map((user) => {
+        return (
+          <p className="my-1" key={user.id}>
+            {user.name}
+          </p>
+        )
+      })}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export default FetchMain
